148393aea3edd39a9f24a6c0859c6a3cba9f17d7
Naveen pandian <npandian515@gmail.com>
2025-03-25 14:31:21 +0530

    implement user authentication with register and login functions; add signOut functionality

--- backend/controllers/AuthControllers.js
+++ backend/controllers/AuthControllers.js
@@ -3,7 +3,7 @@ const {doHash , doHashValidation, hmacProcess} = require("../utils/hashing")
 
 const userModel = require("../models/UserModel")
 
-exports.register = async () => {
+exports.register = async (req , res) => {
     const {name , password , email } = req.body;
     
     try {
@@ -43,6 +43,62 @@ exports.register = async () => {
     }
 };
 
-const login   = async () => {
-    
+const login   = async (req, res) => {
+    const {name , password} = req.body;
+
+    try {
+
+        const existingUser = await userModel.findOne({name }).select("+password");
+         if(!existingUser) {
+             return res.status(400).json({
+                 success : false ,
+                 message : "User does not exist"
+             })      
+               }
+
+               const isPasswordValid = await doHashValidation(password , existingUser.password);
+
+               if(!isPasswordValid){
+                return res.status(401).json({
+                     success :  false , message : "Make sure your Password Valid"
+                })
+               }
+
+               const token = jwt.sign(
+                {
+                    userId : existingUser._id,
+                    name : existingUser.name
+
+                },
+                process.env.SECRET_KEY ,
+                {expiresIn : "1d"}
+               );
+
+               res.cookie("Authorization", `Bearer ${token}`, {
+                httpOnly : true,
+                expires : new Date(Date.now() + 3600000 ),
+                secure : process.env.NODE_ENV === "production",
+               })
+        
+               return res.status(200).json({
+                success : true ,
+                message: "You are in !",
+                token : token,
+               })
+
+    } catch (error) {
+          return res.status(500).json({
+            success : false ,
+            message : "Internal Server Error"
+          })
+    }
+
+};
+
+exports.signOut = async (req,res) => {
+    res.clearCookie("Authorization");
+    res.status(200).json({
+        success : true ,
+        message : "You are logged out !"
+    })
 }
