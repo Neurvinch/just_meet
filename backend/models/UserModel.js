e3041e59d78e13e78b73d908348a657b95093c2c
Naveen pandian <npandian515@gmail.com>
2025-03-25 13:05:28 +0530

    add user model with schema definition for authentication

---
+++ backend/models/UserModel.js
@@ -0,0 +1,55 @@
+const mongoose = require ("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    name : {
+        type : String,
+        required: [true , "Please Enter your Name To Surf "],
+        unique : true,
+
+    },
+
+    email : {
+        type: String,
+        required : [true , "Give me your Email "],
+            unique : true,
+    },
+     password : {
+        type : String,
+        required : [true , "Please Enter your Password "],
+         trim : true,
+         select : false,
+     },
+     avatar : {
+        type : String,
+        default : "default.jpg",
+        
+     },
+     verified : {
+        type : Boolean,
+        default : false,
+     },
+     verificationCode : {
+        type : String,
+        select : false ,
+     },
+
+     verificationCodeValidation : {
+        type : Number,
+        select : false
+     },
+     forgotPasswordCode : {
+        type : String,
+        select : false
+     },
+     forgotPasswordCodeValidation : {
+        type : Number,
+        select : false
+     },
+    
+
+}  ,
+{
+    timestamps : true ,
+}
+)
