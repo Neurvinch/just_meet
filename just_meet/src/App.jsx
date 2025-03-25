f5fe35f904e9612b80b13e52f32b4638b3965034
Naveen pandian <npandian515@gmail.com>
2025-03-25 16:10:11 +0530

    add Register component and integrate it into App; implement registration form with API integration

--- just_meet/src/App.jsx
+++ just_meet/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
 
 
   return (
     <>
+    <Register/>
   <Login/>
     </>
   )
