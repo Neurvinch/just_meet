f833d63b1abbde146572e442bb1fb61f46ac4788
Naveen pandian <npandian515@gmail.com>
2025-03-25 13:16:22 +0530

    do some utils hash process

---
+++ backend/utils/hashing.js
@@ -0,0 +1,17 @@
+const bcrpt = require ("bcryptjs")
+const {createHmac} = require("crypto")
+
+exports.doHash = ( value , saltValue) => {
+  const result = bcrypt.hash(value , saltValue)
+  return result
+}
+
+exports.doHashValidation = (value , hashedValue) =>{
+    const result = bcrpt.compare(value , hashedValue)
+    return result
+}
+
+exports.hmacProcess = (value , key) => {
+    const result = createHmac("sha256" , key).update(value).digest("hex");
+    return result
+}
