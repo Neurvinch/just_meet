4d0030d530cf3f503f745bb33287c5cc12dbe065
thej1812 <thejashwini1800@gmail.com>
2025-03-25 18:46:08 +0530

    app

--- just_meet/src/App.jsx
+++ just_meet/src/App.jsx
@@ -1,28 +1,13 @@
 
 import './App.css'
-<<<<<<< HEAD
 import Environment from './Pages/environment'
 import PixelAuthPage from './Pages/lg'
-=======
-//import ForgotPassword from './pages/ForgotPasssword'
-import Login from './pages/Login'
-import Payement from './pages/Payement'
-import Register from './pages/Register'
-
->>>>>>> 76bbe0ae093370129fd24d7de49e8c19773d2600
 function App() {
 
 
   return (
     <>
-<<<<<<< HEAD
       <Environment/>
-=======
-    {/* <ForgotPassword/> */}
-    <Payement/>
-    <Register/>
-  <Login/>
->>>>>>> 76bbe0ae093370129fd24d7de49e8c19773d2600
     </>
   )
 }
