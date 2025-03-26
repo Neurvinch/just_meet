5dd267a157e8a727bc3add2e077112b11396be16
Naveen pandian <npandian515@gmail.com>
2025-03-26 05:35:31 +0530

    ``` No code changes detected. ```

--- just_meet/src/App.jsx
+++ just_meet/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Mainpage from "./Pages/Mainpage";
@@ -16,35 +15,12 @@ import MathGame from './components/MathGame';
 import SlidePuzzle from './components/SlidePuzzle';
 import TreasureToss from './components/TreasureToss';
 import Login from "./Pages/Login";
-=======
-
-import Announcements from './pages/Announcements'
-import Chat from './pages/Chat'
-//import ForgotPassword from './pages/ForgotPasssword'
-import Login from './pages/Login'
-import Payement from './pages/Payement'
-import Polling from './pages/Polling'
-import Register from './pages/Register'
-import TaskBoard from './pages/TaskBoard'
-import TaskList from './pages/TaskList'
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Mainpage from "./Pages/mainpage";
-import Landing from "./Pages/Landing";
-import EnvironmentPage from "./Pages/environment";
-import PixelAuthPage from "./Pages/PixelAuthPage";
-import Home from "./Pages/Home";
-import Room from "./Pages/Room";
-
->>>>>>> c59185d190828875deff76239ad2aab978ae3c76
 
 function App() {
   return (
 
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Mainpage />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/environment" element={<EnvironmentPage />} />
@@ -60,17 +36,6 @@ function App() {
         <Route path="/slide" element={<SlidePuzzle />} />
         <Route path="/treasure" element={<TreasureToss />} />
         <Route path="/login" element={<Login />} /> {/* ✅ Made lowercase */}
-=======
-        <Route path="/" element={<Mainpage/>} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/environment" element={<EnvironmentPage />} />
-        <Route path="/login" element={<PixelAuthPage/>} />
-        <Route path = "/chat" element = {<Chat/>} />
-        <Route path='/poll' element = {<Polling/>}   />
-        <Route path = "/task" element = { <TaskList/>}  />
-        <Route path = "/payement" element={<Payement/>}   /> 
-
->>>>>>> c59185d190828875deff76239ad2aab978ae3c76
       </Routes>
     </Router>
   );
